f201a57f08512f3f020daedab6dfd13ad2424338
numbers <numbers@dockerhost>
2020-11-12 20:01:22 +0000

    match with dockerhub tag

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 save_use_model.py
-sudo docker build -t use-tf-serving .
-sudo docker run -it --rm  -p 8501:8501 -t use-serving
+sudo docker build -t jefshe/use-tf-serving .
+sudo docker run -it --rm  -p 8501:8501 -t jefshe/use-serving
